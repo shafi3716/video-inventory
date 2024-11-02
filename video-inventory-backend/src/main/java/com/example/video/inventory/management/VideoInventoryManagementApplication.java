32f970852c55420545661be274c6fdaa276566fe
shafi3716 <abdullahalshafi1212@gmail.com>
2024-11-03 00:41:05 +0600

    backend project and dependency initialize

---
+++ video-inventory-backend/src/main/java/com/example/video/inventory/management/VideoInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.video.inventory.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoInventoryManagementApplication.class, args);
+	}
+
+}
---
+++ video-inventory-backend/src/test/java/com/example/video/inventory/management/VideoInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.video.inventory.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoInventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
